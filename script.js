268a037f252aa9af86b42c02cc74a32a3bfaf15a
KevichVinc <kevich.vinc@gmail.com>
2021-02-24 15:47:52 +0300

    second try

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// fetch(`https://rickandmortyapi.com/api/character/?page=1`)
-//   .then((response) => response.json())
-//   .then((data) => makeCards(data.results));
-
 function handleClick(id) {
   console.log(`${id} has been added to cart`);
 }
@@ -16,98 +12,98 @@ const goods = [
   },
   {
     id: `prod${Math.ceil(Math.random() * 10000)}`,
-    name: 'Молоток',
+    name: 'Штукатурка',
     specs:
       'Вес: 3 кг <br> ДхВхШ (см): 30 х 2 х 4 <br> Материалы: дерево, метал',
     price: `${Math.ceil(Math.random() * 100)}`,
   },
   {
     id: `prod${Math.ceil(Math.random() * 10000)}`,
-    name: 'Молоток',
+    name: 'Плинтус',
     specs:
       'Вес: 3 кг <br> ДхВхШ (см): 30 х 2 х 4 <br> Материалы: дерево, метал',
     price: `${Math.ceil(Math.random() * 100)}`,
   },
   {
     id: `prod${Math.ceil(Math.random() * 10000)}`,
-    name: 'Молоток',
+    name: 'Сверла',
     specs:
       'Вес: 3 кг <br> ДхВхШ (см): 30 х 2 х 4 <br> Материалы: дерево, метал',
     price: `${Math.ceil(Math.random() * 100)}`,
   },
   {
     id: `prod${Math.ceil(Math.random() * 10000)}`,
-    name: 'Молоток',
+    name: 'Гвозди',
     specs:
       'Вес: 3 кг <br> ДхВхШ (см): 30 х 2 х 4 <br> Материалы: дерево, метал',
     price: `${Math.ceil(Math.random() * 100)}`,
   },
   {
     id: `prod${Math.ceil(Math.random() * 10000)}`,
-    name: 'Молоток',
+    name: 'Саморезы',
     specs:
       'Вес: 3 кг <br> ДхВхШ (см): 30 х 2 х 4 <br> Материалы: дерево, метал',
     price: `${Math.ceil(Math.random() * 100)}`,
   },
   {
     id: `prod${Math.ceil(Math.random() * 10000)}`,
-    name: 'Молоток',
+    name: 'Плинтус',
     specs:
       'Вес: 3 кг <br> ДхВхШ (см): 30 х 2 х 4 <br> Материалы: дерево, метал',
     price: `${Math.ceil(Math.random() * 100)}`,
   },
   {
     id: `prod${Math.ceil(Math.random() * 10000)}`,
-    name: 'Молоток',
+    name: 'Рулетка',
     specs:
       'Вес: 3 кг <br> ДхВхШ (см): 30 х 2 х 4 <br> Материалы: дерево, метал',
     price: `${Math.ceil(Math.random() * 100)}`,
   },
   {
     id: `prod${Math.ceil(Math.random() * 10000)}`,
-    name: 'Молоток',
+    name: 'Болгарка',
     specs:
       'Вес: 3 кг <br> ДхВхШ (см): 30 х 2 х 4 <br> Материалы: дерево, метал',
     price: `${Math.ceil(Math.random() * 100)}`,
   },
   {
     id: `prod${Math.ceil(Math.random() * 10000)}`,
-    name: 'Молоток',
+    name: 'Пила',
     specs:
       'Вес: 3 кг <br> ДхВхШ (см): 30 х 2 х 4 <br> Материалы: дерево, метал',
     price: `${Math.ceil(Math.random() * 100)}`,
   },
   {
     id: `prod${Math.ceil(Math.random() * 10000)}`,
-    name: 'Молоток',
+    name: 'Топор',
     specs:
       'Вес: 3 кг <br> ДхВхШ (см): 30 х 2 х 4 <br> Материалы: дерево, метал',
     price: `${Math.ceil(Math.random() * 100)}`,
   },
   {
     id: `prod${Math.ceil(Math.random() * 10000)}`,
-    name: 'Молоток',
+    name: 'Кувалда',
     specs:
       'Вес: 3 кг <br> ДхВхШ (см): 30 х 2 х 4 <br> Материалы: дерево, метал',
     price: `${Math.ceil(Math.random() * 100)}`,
   },
   {
     id: `prod${Math.ceil(Math.random() * 10000)}`,
-    name: 'Молоток',
+    name: 'Канцелярский нож',
     specs:
       'Вес: 3 кг <br> ДхВхШ (см): 30 х 2 х 4 <br> Материалы: дерево, метал',
     price: `${Math.ceil(Math.random() * 100)}`,
   },
   {
     id: `prod${Math.ceil(Math.random() * 10000)}`,
-    name: 'Молоток',
+    name: 'Перчатки',
     specs:
       'Вес: 3 кг <br> ДхВхШ (см): 30 х 2 х 4 <br> Материалы: дерево, метал',
     price: `${Math.ceil(Math.random() * 100)}`,
   },
   {
     id: `prod${Math.ceil(Math.random() * 10000)}`,
-    name: 'Молоток',
+    name: 'Малярная лента',
     specs:
       'Вес: 3 кг <br> ДхВхШ (см): 30 х 2 х 4 <br> Материалы: дерево, метал',
     price: `${Math.ceil(Math.random() * 100)}`,
@@ -119,11 +115,13 @@ function makeCards(goods) {
   goods.forEach((item) => {
     productContainer.innerHTML =
       productContainer.innerHTML +
-      `<div class = "product" id="${item.id}">
-         <div class = "name">${item.name}</div>
-         <div class = "specs">${item.specs}</div>
-         <div class = "price">Цена: ${item.price}</div>
-         <button class = "buy" id=${item.id} type= "button" onclick="handleClick(id)"> BUY </button>
+      `<div class="productCell">
+         <div class = "product" id="${item.id}" draggable="true">
+           <div class = "name">${item.name}</div>
+           <div class = "specs">${item.specs}</div>
+           <div class = "price">Цена: ${item.price}</div>
+           <button class = "buy" id=${item.id} type= "button" onclick="handleClick(id)"> BUY </button>
+         </div>
        </div>
     `;
   });
@@ -131,27 +129,48 @@ function makeCards(goods) {
 
 makeCards(goods);
 
-document.querySelectorAll('.product').forEach((e) => {
-  e.draggable = true;
-  e.onDragStart = (e) => {
-    e.dataTransfer.setData('id', e.target.id);
-    e.target.classList.add('dragging');
+const dragAndDrop = () => {
+  const products = document.querySelectorAll('.product');
+  const productCells = document.querySelectorAll('.productCell');
+
+  const dragStart = function (e) {
+    setTimeout(() => {
+      this.classList.add('hide');
+    });
+    e.dataTransfer.setData('src', e.target.id);
   };
-  e.ondragover = (e) => {
-    let old = document.querySelector('.over');
-    old && old.classList.remove('over');
-    e.target.classList.add('over');
+  const dragEnd = function () {
+    this.classList.remove('hide');
+  };
+  const dragOver = function (e) {
     e.preventDefault();
   };
-  e.ondrop = (e) => {
-    let old = document.querySelector('.dragging');
-    old && old.classList.remove('dragging');
-    old = document.querySelector('.over');
-    old && old.classList.remove('over');
-    let v = e.target.innerHTML;
-    console.log(e.target.id);
-    let fromEl = document.querySelector('#' + e.dataTransfer.getData('id'));
-    e.target.innerHTML = fromEl.innerHTML;
-    fromEl.innerHTML = v;
+  const dragEnter = function () {
+    this.classList.add('hovered');
   };
-});
+  const dragLeave = function () {
+    this.classList.remove('hovered');
+  };
+  const dragDrop = function (e) {
+    let dragSrc = document.getElementById(e.dataTransfer.getData('src'));
+    let srcParent = dragSrc.parentNode;
+    let dragTarget = e.currentTarget.firstElementChild;
+
+    e.currentTarget.replaceChild(dragSrc, dragTarget);
+    srcParent.appendChild(dragTarget);
+  };
+
+  productCells.forEach((cell) => {
+    cell.addEventListener('dragover', dragOver);
+    cell.addEventListener('dragenter', dragEnter);
+    cell.addEventListener('dragleave', dragLeave);
+    cell.addEventListener('drop', dragDrop);
+  });
+
+  products.forEach((product) => {
+    product.addEventListener('dragstart', dragStart);
+    product.addEventListener('dragend', dragEnd);
+  });
+};
+
+dragAndDrop();
